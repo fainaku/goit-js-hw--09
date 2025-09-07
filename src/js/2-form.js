9af1769494715a4c010e57da7ca44bb66b37a1a0
Faina K <fayina1@gmail.com>
2025-09-07 15:30:15 +0200

    add gallery and form

---
+++ src/js/2-form.js
@@ -0,0 +1,50 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+
+populateFormFields();
+formEl.addEventListener('input', handleFormInput);
+formEl.addEventListener('submit', handleFormSubmit);
+
+function handleFormInput(event) {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = '';
+  formData.message = '';
+}
+
+function populateFormFields() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (!savedData) {
+    return;
+  }
+  const data = JSON.parse(savedData);
+
+  formData.email = data.email || '';
+  formData.message = data.message || '';
+
+  if (formData.email) {
+    formEl.elements.email.value = formData.email;
+  }
+  if (formData.message) {
+    formEl.elements.message.value = formData.message;
+  }
+}
